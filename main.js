7c9a7b7717575b6b9f3a716b5f598fbf62ac1a3a
Aanya-009 <90621213+Aanya-009@users.noreply.github.com>
2023-01-29 12:30:24 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
-status = false;
+flag = "";
+object = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -13,6 +14,33 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+
+    if(flag != ""){
+        objectDetector.detect(video, gotResult);
+        object_name = document.getElementById("input_box").value;
+        if(object.length > 0){
+            console.log("test");
+            for(i = 0; 1 < object.length; i++){
+                console.log(object[i].label);
+                if(object[i].label == object_name){
+                    document.getElementById("status").innerHTML = "Status: Object Detected";
+                    document.getElementById("number_of_objects").innerHTML = "Total number of objects detected are: " + object.length;
+    
+                    fill("#eb3b4f");
+                    percent = floor(object[i].confidence * 100);
+                    text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+                    noFill();
+                    stroke("#eb3b4f");
+                    rect(object[i].x, object[i].y, object[i].width, object[i].height);
+                }
+                else{
+                    document.getElementById("status").innerHTML = "Status: Object not Detected";
+                    document.getElementById("number_of_objects").innerHTML = "Total number of objects detected are: " + object.length;
+    
+                }
+            }
+        }
+    }
 }
 
 function start(){
@@ -22,8 +50,18 @@ function start(){
 
 function modelLoaded(){
     console.log("model is loaded");
-    status = true;
+    flag = "true";
     video.speed(1);
     video.volume(0);
     video.loop();
+}
+
+function gotResult(error, result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        onject = result;
+    }
 }
